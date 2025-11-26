abd07b81a7349c6f2a95ddc9925d8815a38532f1
RafaelsXavi <rafaelx277@gmail.com>
2025-11-26 00:56:27 -0300

    configuração do banco de dados, instalacao do sequelize

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'admin',
+    password: '081327',
+    database:'devburguer-db',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
+
--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 import app from "./app.js";
 
 
-app.listen(3001, () =>
-    console.log("Servidor rodando na porta 3001"));
+app.listen(3001, () => console.log("Servidor rodando na porta 3001"));
