6f9cc7b0de82b24c20f3ef9aab3490578c529a6e
RafaelsXavi <rafaelx277@gmail.com>
2025-11-27 10:45:10 -0300

    correção de erros

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,18 +1,21 @@
-import Sequelize from "sequelize";
+import Sequelize, { Model } from 'sequelize';
 
-export default class User extends Sequelize.Model {
+
+class User extends Model {
+
     static init(sequelize) {
-        super.init(
+        Model.init(
             {
-                name: { type: Sequelize.STRING, allowNull: false },
-                email: { type: Sequelize.STRING, allowNull: false, unique: true },
-                password_hash: { type: Sequelize.STRING, allowNull: false },
-                admin: { type: Sequelize.BOOLEAN, defaultValue: false },
+                name: Sequelize.STRING,
+                email: Sequelize.STRING,
+                password_hash: Sequelize.STRING,
+                admin: Sequelize.BOOLEAN,
             },
             {
                 sequelize,
-                tableName: "users",
             }
         );
     }
 }
+
+export default User;
