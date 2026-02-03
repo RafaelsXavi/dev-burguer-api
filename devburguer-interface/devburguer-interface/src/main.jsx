8c6dbbdce171b742ea5886e0bd32e310f3f27c7e
RAFAEL <rafaelx277@gmail.com>
2026-02-03 10:22:41 -0300

    feat: adiciona estilos globais e configurações do ESLint e Prettier

--- devburguer-interface/devburguer-interface/src/main.jsx
+++ devburguer-interface/devburguer-interface/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GlobalStyle } from './styles/globalStyles.js'
 
 import App from './App.jsx'
 import { Login } from './containers/Login/index.jsx'
@@ -7,5 +8,6 @@ import { Login } from './containers/Login/index.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Login/>
+    <GlobalStyle />
   </StrictMode>,
 )
